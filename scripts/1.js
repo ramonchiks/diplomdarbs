c80bd13f7392650ee1d8f1b808654c91307e24f1
ramonchiks <73561151+ramonchiks@users.noreply.github.com>
2021-05-07 00:09:28 +0300

    visu iesāku, uztiasīju sākuma tekstu clickable through līdz vietai, kur parādās vektoru acis, protams, tikai angliski (šobrīd)

---
+++ scripts/1.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+
+	$(".hello").click(function(){
+
+		$(".augsejaisteksts").css("display", "inline")
+		$(".yesno, .yes, .no").css("display", "inline-block")
+		$(".hello").css("display", "none")
+
+	});
+
+// apakšā: no yes/no uz attiecīgajiem rezultātiem
+
+
+	$(".yes").click(function(){
+		$(".ifyes").css("display", "inline")
+		$(".yesno, .yes, .no, .augsejaisteksts").css("display", "none")
+	});
+
+	$(".no").click(function(){
+		$(".ifno").css("display", "inline")
+		$(".yesno, .yes, .no, .augsejaisteksts").css("display", "none")
+	});
+
+	$(".ifyes").mouseover(function(){
+		$(".ifyes").html("tHaNks :(");
+	});
+
+//ceļojums sākas!!!
+
+	$(".ifyes").click(function(){
+		$(".story").css("display", "inline");
+		$(".ifyes").css("display", "none");
+
+	});
+
+	$(".story").click(function(){
+		$(".story").html("My generation was one of the first (if not the first) that grew up largely online");
+
+	$(".story").click(function(){
+		$(".story").html("I have many fond memories of nostalgic corners of the web");
+
+	$(".story").click(function(){
+		$(".story").html("Unlike in real life, here they never really disappear");
+
+	$(".story").click(function(){
+		$(".story").html("The rules of time and space are different online");
+
+	$(".story").click(function(){
+		$(".story").html("I don't think people realise quite how much the internet has changed the world");
+
+	$(".story").click(function(){
+		$(".story").html("I don't think that's possible just yet");
+
+	$(".story").click(function(){
+		$(".story").html("Among many other things, it gave a chance for people to realise");
+
+	$(".story").click(function(){
+		$(".story").html("how much easier it can sometimes be to talk to a complete stranger,");
+
+	$(".story").click(function(){
+		$(".story").html("especially if you never have to look them in the eye");
+
+	$(".story").click(function(){
+		$(".story").css("display", "none");
+
+//te jānāk iekšā vektoru acīm
+
+	});
+
+	});
+
+	});
+
+	});
+
+	});
+
+	});
+
+	});
+
+	});
+
+	});
+
+	});
+
+});
+
