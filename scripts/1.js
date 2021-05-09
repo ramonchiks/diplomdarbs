e3d8968ed44ce145cb280d435e6a2942ab975f07
ramonchiks <73561151+ramonchiks@users.noreply.github.com>
2021-05-09 17:31:07 +0300

    ieliku typewriter funkciju sākumam, bet vēl jāsaprot par delays

--- scripts/1.js
+++ scripts/1.js
@@ -1,24 +1,42 @@
 $(document).ready(function(){
+	writethis("Hello!", ".hello")
 
 	$(".hello").click(function(){
 
-		$(".augsejaisteksts").css("display", "block")
-		$(".yesno, .yes, .no").css("display", "inline-block")
-		$(".hello").css("display", "none")
+		writethis("I would like to tell you a story. \n Are you willing to listen?", ".hello")
+		$(".hello").css("font-size", "5vw")
 
+		$(".yes, .no").css("display", "inline-block")
+		writethis1("YES", ".yes")
+		writethis2("NO", ".no")
+		
 	});
+
+
+
+	//$(".hello").click(function(){
+	//	$(".hello").css("display", "none")
+	//})
+
+	/* $(".augsejaisteksts").mouseover(function(){
+		$(".yesno, .yes, .no").css("display", "inline-block")
+		writethis("YES", ".yes")
+		writethis("NO", ".no")
+	}) */
 
 // apakšā: no yes/no uz attiecīgajiem rezultātiem
 
 
 	$(".yes").click(function(){
 		$(".ifyes").css("display", "inline")
-		$(".yesno, .yes, .no, .augsejaisteksts").css("display", "none")
+		writethis("Thanks :)", ".ifyes")
+		$(".yesno, .yes, .no, .hello").css("display", "none")
 	});
 
 	$(".no").click(function(){
 		$(".ifno").css("display", "inline")
-		$(".yesno, .yes, .no, .augsejaisteksts").css("display", "none")
+		writethis(":(", ".ifno")
+		$(".yesno, .yes, .no, .hello").css("display", "none")
 	});
 
 	$(".ifyes").mouseover(function(){
@@ -30,60 +48,63 @@ $(document).ready(function(){
 	$(".ifyes").click(function(){
 		$(".story").css("display", "block");
 		$(".ifyes").css("display", "none");
+		writethis("I have never lived \n in a world without the internet", ".story")
 
 	});
 
-	$(".story").click(function(){
-		$(".story").html("My generation was one of the first (if not the first) that grew up largely online");
+ 	$(".story").click(function(){
+ 		writethis("My generation was one of the first \n (if not the first) \n to grow up largely online", ".story")
 
-	$(".story").click(function(){
-		$(".story").html("I have many fond memories of nostalgic corners of the web");
+ 	$(".story").click(function(){
+ 		writethis("I have many fond memories \n of nostalgic corners of the web", ".story")
 
-	$(".story").click(function(){
-		$(".story").html("Unlike in real life, here they never really disappear");
+ 	$(".story").click(function(){
+ 		writethis("Unlike in real life, \n here they never really disappear", ".story")
 
-	$(".story").click(function(){
-		$(".story").html("The rules of time and space are different online");
+ 	$(".story").click(function(){
+ 		writethis("The rules of time and space \n are different online", ".story")
 
-	$(".story").click(function(){
-		$(".story").html("I don't think people realise quite how much the internet has changed the world");
+ 	$(".story").click(function(){
+ 		writethis("I don't think people realise \n quite how much the internet \n has changed the world", ".story")
 
-	$(".story").click(function(){
-		$(".story").html("I don't think that's possible just yet");
+ 	$(".story").click(function(){
+ 		writethis("I don't think that's possible just yet", ".story")
 
-	$(".story").click(function(){
-		$(".story").html("Among many other things, it gave a chance for people to realise");
+ 	$(".story").click(function(){
+ 		writethis("Among many other things, \n it gave a chance for people to realise", ".story")
 
-	$(".story").click(function(){
-		$(".story").html("how much easier it can sometimes be to talk to a complete stranger,");
+ 	$(".story").click(function(){
+ 		writethis("how much easier it can sometimes be \n to talk to a complete stranger,", ".story")
 
-	$(".story").click(function(){
-		$(".story").html("especially if you never have to look them in the eye");
+ 	$(".story").click(function(){
+ 		writethis("especially if you never have \n to look them in the eye", ".story")
 
-	$(".story").click(function(){
-		$(".story").css("display", "none");
+ 	$(".story").click(function(){
+ 		$(".story").css("display", "none")
 
 //te jānāk iekšā vektoru acīm
 
-	});
+ 	})
 
-	});
+ 	})
 
-	});
+ 	})
 
-	});
+ 	})
 
-	});
+ 	})
 
-	});
+ 	})
 
-	});
+ 	})
 
-	});
+ 	})
 
-	});
+ 	})
 
-	});
+ 	})
+
+
 
 });
 
--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -1,15 +1,57 @@
+
+
+var interval;
 function writethis(text, element) {
-	$(element).html('') // idzest iepriekšējo saturu
-	typeWriter(text, element, 0); // izsaukt rekursīvu funkciju, kura raksta burtu pa burtam
+  var speed = 50
+  var position = 0
+  
+  clearInterval(interval); // ja iepriekš kaut kas uzsākts - to apstādinam, pirms saākam jana teksta uzrakstīšanu
+  $(element).html('') // idzest iepriekšējo saturu
+  
+  interval = setInterval(function(){
+    if (position < text.length) {
+      document.querySelector(element).innerHTML += text.charAt(position);
+      position++;
+    } else {
+      clearInterval(interval);
+    }
+  }, speed);
 }
 
-function typeWriter(text, element ,position) {
-	var speed = 50 
-  if (position < text.length) {
-    document.querySelector(element).innerHTML += text.charAt(position);
-    position++;
-    setTimeout(function(){
-			typeWriter(text, element, position)
-		}, speed);
-  }
+
+
+var interval1;
+function writethis1(text, element) {
+  var speed1 = 50
+  var position1 = 0
+  
+  clearInterval(interval1); // ja iepriekš kaut kas uzsākts - to apstādinam, pirms saākam jana teksta uzrakstīšanu
+  $(element).html('') // idzest iepriekšējo saturu
+  
+  interval1 = setInterval(function(){
+    if (position1 < text.length) {
+      document.querySelector(element).innerHTML += text.charAt(position1);
+      position1++;
+    } else {
+      clearInterval(interval1);
+    }
+  }, speed1);
+}
+
+var interval2;
+function writethis2(text, element) {
+  var speed2 = 50
+  var position2 = 0
+  
+  clearInterval(interval2); // ja iepriekš kaut kas uzsākts - to apstādinam, pirms saākam jana teksta uzrakstīšanu
+  $(element).html('') // idzest iepriekšējo saturu
+  
+  interval2 = setInterval(function(){
+    if (position2 < text.length) {
+      document.querySelector(element).innerHTML += text.charAt(position2);
+      position2++;
+    } else {
+      clearInterval(interval2);
+    }
+  }, speed2);
 }
