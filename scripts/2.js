8c8a693165953e8b41cb956988f70b7cf11b6e87
ramonchiks <73561151+ramonchiks@users.noreply.github.com>
2021-05-10 21:40:05 +0300

    uztaisīju 2eng js un css failus, uztaisīju visu tekstu writethis Kas ir eng2

---
+++ scripts/2.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	writethis("One of the greatest charms \n of the internet", ".story")
+
+	$(".story").click(function(){
+		writethis("is that you get to build \n a world of your own", ".story")
+
+	$(".story").click(function(){
+		writethis("In the wonderfully nonphysical \n space of the web", ".story")
+
+	$(".story").click(function(){
+		writethis("you get to build yourself anew", ".story")
+
+	$(".story").click(function(){
+		writethis("you get to choose what to show", ".story")
+
+	$(".story").click(function(){
+		writethis("and what to keep hidden", ".story")
+
+	$(".story").click(function(){
+		writethis("It isn't hard to see how tempting \n that becomes for someone wishing \n to escape reality", ".story")
+
+	$(".story").click(function(){
+		writethis("and so I did", ".story")
+
+	})
+
+	})
+
+	})
+
+	})
+
+	})
+
+	})
+
+	})
+})
