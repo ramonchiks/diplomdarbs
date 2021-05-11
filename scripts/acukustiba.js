bc91d6e6e97a522c35c3a0264cf4f1778a05d4fe
ramonchiks <73561151+ramonchiks@users.noreply.github.com>
2021-05-11 15:11:17 +0300

    ieliku pareizos svg failus un sapratu, kaa tajos orienteties, meginaju kopejot un mainot panakt acu sekosanas efektu, bet īsti nesanāca, plus rotācija laikam nav the way to go - pie šī jāatgriežas

---
+++ scripts/acukustiba.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+
+
+/*
+
+$("body").mousemove(function(event) {
+  var eye = $(".ieksa");
+  var x = (eye.offset().left) + (eye.width() / 2);
+  var y = (eye.offset().top) + (eye.height() / 2);
+  var rad = Math.atan2(event.pageX - x, event.pageY - y);
+  var rot = (rad * (180 / Math.PI) * -1) + 180;
+  eye.css({
+    '-webkit-transform': 'rotate(' + rot + 'deg)',
+    '-moz-transform': 'rotate(' + rot + 'deg)',
+    '-ms-transform': 'rotate(' + rot + 'deg)',
+    'transform': 'rotate(' + rot + 'deg)'
+  });
+});
+
+*/
+
+
+$("body").mousemove(function(event) {
+
+  var ieksa = $(".ieksa");
+  var arpuse = $(".apkart");
+  var x = (ieksa.offset().left) + (ieksa.width() / 2);
+  var y = (ieksa.offset().top) + (ieksa.height() / 2);
+  var rad = Math.atan2(event.pageX - x, event.pageY - y);
+  var rot = (rad * (180 / Math.PI) * -1) + 180;
+  ieksa.css({
+    '-webkit-transform': 'rotate(' + rot + 'deg)',
+    '-moz-transform': 'rotate(' + rot + 'deg)',
+    '-ms-transform': 'rotate(' + rot + 'deg)',
+    'transform': 'rotate(' + rot + 'deg)'
+  });
+});
+
+})
