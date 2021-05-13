eb27d942bfd6d5f0dbd007d4bd9de16f076eed1c
ramonchiks <73561151+ramonchiks@users.noreply.github.com>
2021-05-13 13:05:56 +0300

    sataisīju puķu augšanu it kā visu, izveidoju 5ENG html js css failus, mainītas arī 2 pēdējās puķu bildes

--- scripts/4.js
+++ scripts/4.js
@@ -2,30 +2,68 @@ $(document).ready(function(){
 	writethis("When I was thirteen \n I moved to the internet", ".story")
 
 	$(".story").click(function(){
-		writethis("I found a space \n where I didn't know anyone, \n and where no one knew me", ".story")
+		writethis("I found a space \n where I didn't know anyone,", ".story")
 
 	$(".story").click(function(){
-		writethis("and I started building myself \n from the ground up", ".story")
-		$("#viens").removeClass(".transparent")
-		$("#viens").addClass(".visible")
+		writethis("and where no one \n knew me", ".story")
+
 
 	$(".story").click(function(){
-		writethis("And, without really noticing it", ".story")
+		$(".story").css("margin-top", "11%")
+		writethis("and I started \n building myself \n from the ground up", ".story")
+		$("#viens").removeClass("transparent")
+		$("#viens").addClass("visible")
 
 	$(".story").click(function(){
-		writethis("my reality quickly became focused \n on crafting an image of myself ", ".story")
+		$(".story").css("margin-top", "11%")
+		writethis("And, without really \n noticing it", ".story")
+		$("#viens").removeClass("visible")
+		$("#viens").addClass("transparent2")
+		$("#divi").removeClass("transparent2")
+		$("#divi").addClass("visible2")
 
 	$(".story").click(function(){
-		writethis("for people I didn't know to look at", ".story")
+		$(".story").css("margin-top", "8%")
+		writethis("my reality \n quickly became focused \n on crafting an image of myself ", ".story")
+		$("#divi").removeClass("visible2")
+		$("#divi").addClass("transparent2")
+		$("#tris").removeClass("transparent2")
+		$("#tris").addClass("visible2")
 
 	$(".story").click(function(){
-		writethis("Every word I said and \n every picture I posted", ".story")
+		writethis("for people I didn't know \n to look at", ".story")
+		$(".story").css("margin-top", "6%")
+		$("#tris").removeClass("visible2")
+		$("#tris").addClass("transparent2")
+		$("#cetri").removeClass("transparent2")
+		$("#cetri").addClass("visible2")
 
 	$(".story").click(function(){
+		writethis("Every word I said \n and every picture I posted", ".story")
+		$(".story").css("margin-top", "1.2%")
+		$("#cetri").removeClass("visible2")
+		$("#cetri").addClass("transparent2")
+		$("#pieci").removeClass("transparent2")
+		$("#pieci").addClass("visible2")
+
+	$(".story").click(function(){
+		$(".story").css("margin-top", "1.2%")
 		writethis("was to build towards a weird \n romanticised version of myself", ".story")
+		$("#pieci").removeClass("visible2")
+		$("#pieci").addClass("transparent2")
+		$("#sesi").removeClass("transparent2")
+		$("#sesi").addClass("visible2")
+
 
 	$(".story").click(function(){
-		writethis("that was never really real", ".story")
+		$(".story").css("display", "none")
+		$(".links").css("display", "block")
+		writethis("that was never really real", ".links")
+		$("#sesi").removeClass("visible2")
+		$("#sesi").addClass("transparent2")
+		$("#septini").removeClass("transparent2")
+		$("#septini").addClass("visible2")
+	})
 
 	})
 
