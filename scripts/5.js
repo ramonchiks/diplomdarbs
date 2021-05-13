1239fb3c5bcdf18e2ab6851b0350690d67b30593
ramonchiks <73561151+ramonchiks@users.noreply.github.com>
2021-05-13 15:10:24 +0300

    iesāku taisīt 5eng

---
+++ scripts/5.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+	writethis("It is hard to describe \n how much I trusted the internet", ".story")
+
+	$(".story").click(function(){
+		writethis("And the more I gave, \n the more I got back", ".story")
+
+	$(".story").click(function(){
+		writethis("I put my diary online", ".story")
+
+	$(".story").click(function(){
+		writethis("and people listened in return", ".story")
+
+	$(".story").click(function(){
+		writethis("Over time, it got to a point", ".story")
+
+	$(".story").click(function(){
+		writethis("where hundreds, sometimes even \nthousands of people", ".story")
+
+	$(".story").click(function(){
+		writethis("would read my diary every day", ".story")
+
+	$(".story").click(function(){
+		writethis("and give me attention and reassurance", ".story")
+	$(".story").click(function(){
+		writethis("like a great, shapeless mass of support", ".story")
+	$(".story").click(function(){
+		writethis("that never challenged me", ".story")
+
+	$(".story").click(function(){
+		writethis("a pseudo-social relationship like that \n is awfully comfortable", ".story")
+
+	$(".story").click(function(){
+		writethis("it is missing everything \n that makes interactions in real life \n difficult", ".story")
+
+	})	
+
+	})	
+
+	})
+
+	})
+
+	})	
+
+	})
+
+	})
+
+	})
+
+	})
+
+	})
+
+	})
+})
