84f52a95f1c1c122f1de11a6f5dda6ce32bb16a6
ramonchiks <73561151+ramonchiks@users.noreply.github.com>
2021-05-21 04:45:36 +0300

    UZTAISĪJU STILĪGU SĀKUMA VALODAS IZVĒLES TEKSTU

---
+++ scripts/sakums.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+		$(".lv").mouseover(function(){
+			$(".lv").css("mix-blend-mode", "normal")
+
+		})
+
+		$(".lv").mouseleave(function(){
+			$(".lv").css("mix-blend-mode", "screen")
+
+		})
+
+
+		$(".eng").mouseover(function(){
+			$(".eng").css("mix-blend-mode", "normal")
+
+		})
+
+		$(".eng").mouseleave(function(){
+			$(".eng").css("mix-blend-mode", "screen")
+
+		})
+
+
+
+	});
