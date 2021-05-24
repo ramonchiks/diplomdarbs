cbbbc8d6f8def43674fb881ee867bebe881ce638
ramonchiks <73561151+ramonchiks@users.noreply.github.com>
2021-05-24 22:00:49 +0300

    uztaisīju acīm zīlītes, kas arī kustas

--- scripts/acukustiba.js
+++ scripts/acukustiba.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+
   $("body").mousemove(function(event) {
     var screenw = $( window ).width();
     $(".ieksa").each(function() {
@@ -11,4 +13,20 @@ $(document).ready(function(){
       // console.log(this, eyeposy, (event.pageY-eyeposy)/20)
     })
   });
+
+
+  $("body").mousemove(function(event) {
+    var screenw = $( window ).width();
+    $(".zilite").each(function() {
+      var zilite = $(this);
+      var eyeposx = (zilite.offset().left) + (zilite.width() / 2);
+      var eyeposy = (zilite.offset().top) + (zilite.height() / 2);
+      zilite.css({
+        'transform': 'translateX(' + (event.pageX-eyeposx)/17 + 'px) translateY(' + (event.pageY-eyeposy)/15 + 'px)'
+      });
+      // console.log(this, eyeposy, (event.pageY-eyeposy)/20)
+    })
+  });
+
+
 })
