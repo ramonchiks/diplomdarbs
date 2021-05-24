0a2aa928d3ab67cc5434aea0a8aaeb7d5584f495
ramonchiks <73561151+ramonchiks@users.noreply.github.com>
2021-05-24 21:23:36 +0300

    salaboju pukes LOL vinas bija tik insane saliktas

--- scripts/4.js
+++ scripts/4.js
@@ -11,58 +11,49 @@ $(document).ready(function(){
 	$(".story").click(function(){
 		$(".story").css("margin-top", "11%")
 		writethis("and I started \n building myself \n from the ground up", ".story")
-		$("#viens").removeClass("transparent")
-		$("#viens").addClass("visible")
+		$("#viens").css("opacity", "1")
+
 
 	$(".story").click(function(){
 		$(".story").css("margin-top", "11%")
 		writethis("And, without really \n noticing it", ".story")
-		$("#viens").removeClass("visible")
-		$("#viens").addClass("transparent2")
-		$("#divi").removeClass("transparent2")
-		$("#divi").addClass("visible2")
+		$("#zeme").css("opacity", "1")
+		$("#viens").css("opacity", "0")
+		$("#divi").css("opacity", "1")
 
 	$(".story").click(function(){
 		$(".story").css("margin-top", "10%")
 		writethis("my reality \n quickly became focused \n on crafting an image of myself ", ".story")
-		$("#divi").removeClass("visible2")
-		$("#divi").addClass("transparent2")
-		$("#tris").removeClass("transparent2")
-		$("#tris").addClass("visible2")
+		$("#divi").css("opacity", "0")
+		$("#tris").css("opacity", "1")
 
 	$(".story").click(function(){
 		writethis("for people I didn't know \n to look at", ".story")
 		$(".story").css("margin-top", "8%")
-		$("#tris").removeClass("visible2")
-		$("#tris").addClass("transparent2")
-		$("#cetri").removeClass("transparent2")
-		$("#cetri").addClass("visible2")
+		$("#tris").css("opacity", "0")
+		$("#cetri").css("opacity", "1")
+
 
 	$(".story").click(function(){
 		writethis("Every word I said \n and every picture I posted", ".story")
 		$(".story").css("margin-top", "4%")
-		$("#cetri").removeClass("visible2")
-		$("#cetri").addClass("transparent2")
-		$("#pieci").removeClass("transparent2")
-		$("#pieci").addClass("visible2")
+		$("#cetri").css("opacity", "0")
+		$("#pieci").css("opacity", "1")
 
 	$(".story").click(function(){
 		$(".story").css("margin-top", "4%")
 		writethis("was to build towards a weird \n romanticised version of myself", ".story")
-		$("#pieci").removeClass("visible2")
-		$("#pieci").addClass("transparent2")
-		$("#sesi").removeClass("transparent2")
-		$("#sesi").addClass("visible2")
+		$("#pieci").css("opacity", "0")
+		$("#sesi").css("opacity", "1")
 
 
 	$(".story").click(function(){
 		$(".story").css("display", "none")
 		$(".links").css("display", "block")
 		writethis("that was never really real", ".links")
-		$("#sesi").removeClass("visible2")
-		$("#sesi").addClass("transparent2")
-		$("#septini").removeClass("transparent2")
-		$("#septini").addClass("visible2")
+		$("#sesi").css("opacity", "0")
+		$("#septini").css("opacity", "1")
+
 	})
 
 	})
