bbcdea2ce1bf02814f6f2d82d73bd4d07dd190f9
ramonchiks <73561151+ramonchiks@users.noreply.github.com>
2021-05-24 18:12:42 +0300

    uztiasīju acis tā, lai strādā, ieliku upīša kodu un nomainīju svg failus

--- scripts/acukustiba.js
+++ scripts/acukustiba.js
@@ -1,40 +1,14 @@
 $(document).ready(function(){
-
-
-
-/*
-
-$("body").mousemove(function(event) {
-  var eye = $(".ieksa");
-  var x = (eye.offset().left) + (eye.width() / 2);
-  var y = (eye.offset().top) + (eye.height() / 2);
-  var rad = Math.atan2(event.pageX - x, event.pageY - y);
-  var rot = (rad * (180 / Math.PI) * -1) + 180;
-  eye.css({
-    '-webkit-transform': 'rotate(' + rot + 'deg)',
-    '-moz-transform': 'rotate(' + rot + 'deg)',
-    '-ms-transform': 'rotate(' + rot + 'deg)',
-    'transform': 'rotate(' + rot + 'deg)'
+  $("body").mousemove(function(event) {
+    var screenw = $( window ).width();
+    $(".ieksa").each(function() {
+      var ieksa = $(this);
+      var eyeposx = (ieksa.offset().left) + (ieksa.width() / 2);
+      var eyeposy = (ieksa.offset().top) + (ieksa.height() / 2);
+      ieksa.css({
+        'transform': 'translateX(' + (event.pageX-eyeposx)/20 + 'px) translateY(' + (event.pageY-eyeposy)/17 + 'px)'
+      });
+      // console.log(this, eyeposy, (event.pageY-eyeposy)/20)
+    })
   });
-});
-
-*/
-
-
-$("body").mousemove(function(event) {
-
-  var ieksa = $(".ieksa");
-  var arpuse = $(".apkart");
-  var x = (ieksa.offset().left) + (ieksa.width() / 2);
-  var y = (ieksa.offset().top) + (ieksa.height() / 2);
-  var rad = Math.atan2(event.pageX - x, event.pageY - y);
-  var rot = (rad * (180 / Math.PI) * -1) + 180;
-  ieksa.css({
-    '-webkit-transform': 'rotate(' + rot + 'deg)',
-    '-moz-transform': 'rotate(' + rot + 'deg)',
-    '-ms-transform': 'rotate(' + rot + 'deg)',
-    'transform': 'rotate(' + rot + 'deg)'
-  });
-});
-
 })
