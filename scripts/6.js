dc96c58cfcfb52b99ec88ea5c326f8e194e45ecc
ramonchiks <73561151+ramonchiks@users.noreply.github.com>
2021-05-26 18:38:42 +0300

    iesāku uzfrišināt lillā džeku

--- scripts/6.js
+++ scripts/6.js
@@ -1,71 +1,100 @@
 $(document).ready(function(){
-	writethis("Making a friend in real life \n takes a lot of time", ".story")
+	$(".dzeks1").css("bottom", "-10%")
+
+	setTimeout(function(){
+		writethis("Making a friend in real life \n takes a lot of time", ".story")
+	}, 1000)
 
 	$(".story").click(function(){
 		writethis("energy", ".story")
-		$("#viens").removeClass("visible")
+		$("#viens").removeClass("izlien")
 		$("#viens").addClass("transparent")
-		$("#divi").removeClass("transparent")
-		$("#divi").addClass("visible")
+		$("#viens").css("opacity", "0")
+		$("#divi").css("opacity", "1")
 
 	$(".story").click(function(){
 		writethis("honesty", ".story")
-		$("#divi").removeClass("visible")
-		$("#divi").addClass("transparent")
-		$("#tris").removeClass("transparent")
-		$("#tris").addClass("visible")
+		$("#divi").css("opacity", "0")
+		$("#tris").css("opacity", "1")
 
 	$(".story").click(function(){
 		writethis("and vulnerability", ".story")
-		$("#tris").removeClass("visible")
-		$("#tris").addClass("transparent")
-		$("#cetri").removeClass("transparent")
-		$("#cetri").addClass("visible")
+		$("#tris").css("opacity", "0")
+		$("#cetri").css("opacity", "1")
 
 	$(".story").click(function(){
-		writethis("But once you have \n the constant easy flow of validation", ".story")
+		writethis("But once you have \n the constant easy flow  \n of validation", ".story")
+		setTimeout(function(){
+			$("#chuskabox").addClass("chuskaieksa")
+			$("#zvaigznebox").addClass("zvaigzneieksa")
+			$("#pilebox").addClass("pileieksa")
+			$("#zivsbox").addClass("zivsieksa")
+			$("#zirneklisbox").addClass("zirneklisieksa")
+			$("#zakisbox").addClass("zakisieksa")
+		}, 2100)
+
 	$(".story").click(function(){
 		writethis("that doesn't \n challenge you", ".story")
 
 	$(".story").click(function(){
-		writethis("deliberately putting yourself \n through the emotional hassle \n of real relationships ", ".story")
+		writethis("deliberately \n  putting yourself \n through the emotional hassle", ".story")
+
+	$(".story").click(function(){
+		writethis("of real \n relationships ", ".story")
 
 	$(".story").click(function(){
 		writethis("seems \n counter-intuitive", ".story")
-		$("#cetri").removeClass("visible")
-		$("#cetri").addClass("transparent")
-		$("#pieci").removeClass("transparent")
-		$("#pieci").addClass("visible")
+		$("#cetri").css("opacity", "0")
+		$("#pieci").css("opacity", "1")
 
 	$(".story").click(function(){
 		writethis("or at least it does \n when you're thirteen", ".story")
 
 	$(".story").click(function(){
-		$(".fons").css("background-color", "#151512")
-		$(".story").css("color", "#FFFFFF")
-		$(".dzeks5").addClass("mazaks")
-		writethis("The following isolation \n and failure to deal \n with real human issues", ".story")
-
-	$(".story").click(function(){
-		$(".dzeks5").addClass("uzleju")
-		writethis("only pulls you deeper \n in the bog", ".story")
-
-	$(".story").click(function(){
-		writethis("Relying on the love of people \n who only really exist in your head", ".story")
-
-	$(".story").click(function(){
-		writethis("is very fun", ".story")
-
-	$(".story").click(function(){
-		writethis("but not sustainable", ".story")
-
-	$(".story").click(function(){
-		writethis("I really was left quite alone", ".story")
-
-	$(".story").click(function(){
 		$(".story").css("display", "none")
+		$(".fons").css("background-color", "#151512")
+
+		setTimeout(function(){
+		$(".visible").css("opacity", "0")
+		}, 500)
+
+		setTimeout(function(){
+			$(".dzeks5").addClass("mazaks")
+		}, 3000)
+
+		setTimeout(function(){
+			$(".story3").css("display", "block")
+		writethis("The following \n isolation", ".story3")
+		}, 6000)
+
+		$(".story3").click(function(){
+		writethis("and failure to deal \n with real human issues", ".story3")
+
+
+	$(".story3").click(function(){
+		$(".dzeks5").addClass("uzleju")
+		writethis("only pulls you deeper \n in the bog", ".story3")
+
+	$(".story3").click(function(){
+		writethis("Relying on the love of people \n who only really exist in your head", ".story3")
+
+	$(".story3").click(function(){
+		writethis("can be fun", ".story3")
+
+	$(".story3").click(function(){
+		writethis("but not sustainable", ".story3")
+
+	$(".story3").click(function(){
+		writethis("I really was left quite alone", ".story3")
+
+	$(".story3").click(function(){
+		$(".story3").css("display", "none")
 		$(".story2").css("display", "block")
 		writethis("at least with the problems \n I found difficult to talk about", ".story2")
+
+	})
+
+	})
 
 	})
 
